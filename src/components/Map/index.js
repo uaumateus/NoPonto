f163f7c41fbd7fbe9a3a94c144298bbfc7a71149
Mateus Santos <mateus.pereira99@hotmail.com>
2019-09-24 09:46:08 -0300

    Initial Commit

---
+++ src/components/Map/index.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import MapView from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
+
+export default class Map extends Component {
+
+    state = {
+        region: null
+    }
+
+    componentDidMount(){
+        Geolocation.getCurrentPosition(
+            ({ coords: { latitude, longitude } }) => {
+                        this.setState({ 
+                            region: { 
+                                latitude, 
+                                longitude, 
+                                latitudeDelta: 0.0143, 
+                                longitudeDelta: 0.0134},
+                        })
+            },
+            (error) => {
+                console.log(error.code, error.message);
+            },
+            { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+        );
+    }
+
+    render() {
+        const { region } = this.state;
+
+        return (
+            <View style={{ flex: 1 }}>
+                <MapView 
+                    style={{ flex: 1 }}
+                    region={region}
+                    showsUserLocation={true}
+                    showsMyLocationButton={true}
+                    loadingEnable={true}
+                />
+            </View>
+        );
+    }
+}
